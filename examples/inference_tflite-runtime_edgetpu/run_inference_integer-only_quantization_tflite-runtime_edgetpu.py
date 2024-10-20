37cfcaf0c167affdfd2fa7d61b870a56fda2f04f
mmr689 <miguelmolinarotger@gmail.com>
2024-10-20 19:14:56 +0200

    testing edgetpu

--- examples/inference_tflite-runtime_edgetpu/run_inference_integer-only_quantization_tflite-runtime_edgetpu.py
+++ examples/inference_tflite-runtime_edgetpu/run_inference_integer-only_quantization_tflite-runtime_edgetpu.py
@@ -4,13 +4,14 @@ import numpy as np
 from tflite_runtime.interpreter import Interpreter, load_delegate
 
 image_path = 'assets/dog.jpg'
-model_path = 'results/mobilenet/tf_saved_model/mobilenet_integer_only_quant.tflite'
+# model_path = 'results/mobilenet/tf_saved_model/mobilenet_integer_only_quant.tflite'
+model_path = 'mobilenet_integer_only_quant.tflite'
 labels_path = 'assets/imagenet-simple-labels.txt'
 
 # 1. Cargar el modelo TFLite
 start_load_time = time.time()
-# interpreter = Interpreter(model_path, experimental_delegates=[load_delegate('libedgetpu.so.1')])
-interpreter = Interpreter(model_path, experimental_delegates=[load_delegate('libedgetpu.so.1', {'device': 'usb'})])
+interpreter = Interpreter(model_path, experimental_delegates=[load_delegate('libedgetpu.so.1')])
+# interpreter = Interpreter(model_path, experimental_delegates=[load_delegate('libedgetpu.so.1', {'device': 'usb'})])
 interpreter.allocate_tensors()
 load_time = time.time() - start_load_time
 
