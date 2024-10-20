831d51fb45173e913b13b4609b6b6b44eee7f9e0
mmr689 <miguelmolinarotger@gmail.com>
2024-10-20 16:36:02 +0200

    testing edgetpu

---
+++ test_coral_2_3.py
@@ -0,0 +1,17 @@
+"""full_integer"""
+import time
+import cv2
+import numpy as np
+from tflite_runtime.interpreter import Interpreter, load_delegate
+
+image_path = 'assets/dog.jpg'
+model_path='mobilenet_full_integer_quant.tflite'
+labels_path = 'assets/imagenet-simple-labels.txt'
+
+# 1. Cargar el modelo TFLite
+start_load_time = time.time()
+interpreter = Interpreter(model_path, experimental_delegates=[load_delegate('libedgetpu.so.1')])
+interpreter.allocate_tensors()
+load_time = time.time() - start_load_time
+
+print(load_time)
